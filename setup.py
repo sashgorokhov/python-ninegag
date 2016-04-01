b946768acb8c9e34dbb72cb6d3bc33a7e67f4548
sashgorokhov <sashgorokhov@gmail.com>
2016-04-01 21:14:07 +0300

    Add download url and long description

--- setup.py
+++ setup.py
@@ -1,12 +1,17 @@
 from distutils.core import setup
 
+with open('README.md') as file:
+    long_description = file.read()
+
 setup(
     requires=['beautifulsoup4', 'requests'],
     name='python-ninegag',
     version='0.1',
     py_modules=['pyninegag'],
     url='https://github.com/sashgorokhov/python-ninegag',
-    license='MIT',
+    download_url='https://github.com/sashgorokhov/python-ninegag/archive/master.zip',
+    long_description=long_description,
+    license='MIT License',
     author='sashgorokhov',
     author_email='sashgorokhov@gmail.com',
     description='Python library to get stuff from 9gag.com'
