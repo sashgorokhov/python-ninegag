72454110f29d2d20eb31e93f64b9ef181248927b
sashgorokhov <sashgorokhov@gmail.com>
2016-04-01 22:55:20 +0300

    Forgot to supply README.rst in distribution

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.rst') as file:
 setup(
     requires=['beautifulsoup4', 'requests'],
     name='python-ninegag',
-    version='0.1',
+    version='0.3.1',
     py_modules=['pyninegag'],
     url='https://github.com/sashgorokhov/python-ninegag',
     download_url='https://github.com/sashgorokhov/python-ninegag/archive/master.zip',
